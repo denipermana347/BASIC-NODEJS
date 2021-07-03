fbb32250b5d54e594c23e071b71d63b9a2d6c1eb
denipermana347 <permaden97@gmail.com>
2021-07-03 23:57:37 +0700

    latihan readfile

--- filesystem/index.js
+++ filesystem/index.js
@@ -1,2 +1,13 @@
 const fs = require('fs');
-console.log(fs);
+const { Z_DATA_ERROR } = require('zlib');
+
+const fileReadCallback = (error, data)=>{
+    if(error){
+        console.log(`failed to read file`);
+        return
+    }
+    
+    console.log(data);
+}
+
+fs.readFile('filesystem/todo.txt', 'UTF-8', fileReadCallback);
