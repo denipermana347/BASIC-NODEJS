1bf4a965d3e526cb5598a22118ba23b39b4b64e1
denipermana347 <permaden97@gmail.com>
2021-07-04 08:55:11 +0700

    output.txt

--- writeable/index.js
+++ writeable/index.js
@@ -3,4 +3,4 @@ const TulisData = fs.createWriteStream('output.txt');
 
 writableStream.write('Ini merupakan teks baris pertama!\n');
 writableStream.write('Ini merupakan teks baris kedua!\n');
-writableStream.end('Akhir dari writable stream!');
+writableStream.end();
